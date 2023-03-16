0e3fb54d27d2c36358f8a581f24d4e4e4e24867e
nazero3 <nazerrabah1994@gmail.com>
2023-03-16 13:03:32 +0300

    the assignment is done

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+//first question
+let num1 = 31;
+let num2 = 2;
+let multiply = 31 * 2;
+multiply;
+//second question
+let random = Math.ceil(Math.random() * 10);
+random;
+//third question
+let num3 = 10;
+let num4 = 6;
+let mod = num3 % num4;
+
+//fourth question
+let max = Math.max(20, -1);
+max;
